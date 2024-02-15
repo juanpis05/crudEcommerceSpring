b4e432589221d220de68401b6adbcdc0be96120d
juanpis05 <ninorojasj@gmail.com>
2024-02-15 10:32:16 -0500

    V0.1_EstructuraInicial&ClasesModel

---
+++ src/main/java/com/example/demo/model/usuario.java
@@ -0,0 +1,112 @@
+package com.example.demo.model;
+
+public class usuario {
+
+	private Integer id;
+	private String nombre;
+	private String documento;
+	private String username;
+	private String email;
+	private String direccion;
+	private String telefono;
+	private String tipo;// rol a ocupar el usuario
+	private String password;
+
+	public usuario() {
+
+	}
+
+	public usuario(Integer id, String nombre, String documento, String username, String email, String direccion,
+			String telefono, String tipo, String password) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.documento = documento;
+		this.username = username;
+		this.email = email;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.tipo = tipo;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "usuario [id=" + id + ", nombre=" + nombre + ", documento=" + documento + ", username=" + username
+				+ ", email=" + email + ", direccion=" + direccion + ", telefono=" + telefono + ", tipo=" + tipo
+				+ ", password=" + password + "]";
+	}
+
+}
